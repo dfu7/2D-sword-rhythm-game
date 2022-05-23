5f6d530271740cf27bdc5a19d1061d8fb04b3c82
dfu7 <78887201+dfu7@users.noreply.github.com>
2022-05-23 15:29:33 -0700

    score & combo; still needs to destroy object

---
+++ sword rhythm game/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public int combo = 0;
+    private float pointMultiplier = 1;
+    private float extraPoints = 0;
+    public int comboStart = 2;
+    public int almostPoints = 500;
+    public int perfectPoints = 1000;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI comboText;
+
+    private void OnEnable()
+    {
+        PlayerControls.hitAcc += ConvertScore;
+    }
+
+    private void OnDisable()
+    {
+        PlayerControls.hitAcc -= ConvertScore;
+    }
+
+    void ConvertScore(string hitAcc)
+    {
+        if (hitAcc == "Miss")
+        {
+            ResetCombo();
+        }
+        else if (hitAcc == "Almost")
+        {
+            ResetCombo();
+            score += almostPoints;
+        }
+        else if (hitAcc == "Perfect")
+        {
+            combo += 1;
+
+            if (combo >= comboStart)
+            {
+                pointMultiplier += 0.1f;
+                extraPoints = pointMultiplier * 10;
+            }
+
+            score += perfectPoints + (int)extraPoints;
+        }
+
+        scoreText.SetText(score.ToString() + " pts");
+        comboText.SetText("Combo: " + combo.ToString());
+    }
+
+    void ResetCombo()
+    {
+        combo = 0;
+        pointMultiplier = 1;
+        extraPoints = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
