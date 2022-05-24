6837e87f5d3c718dd8fd3ec3dbac9042bd8a226a
HSDrummond <hsdrum1@gmail.com>
2022-05-24 13:52:35 -0700

    Perfect and Almost Effects

--- sword rhythm game/Assets/Scripts/Yokai/Monster.cs
+++ sword rhythm game/Assets/Scripts/Yokai/Monster.cs
@@ -12,6 +12,30 @@ public class Monster : MonoBehaviour
 
     private Conductor m_conductor;
 
+    public ParticleSystem pSplatter;
+    public GameObject inkSplatter;
+
+    private void OnEnable()
+    {
+        PlayerControls.hitAcc += CreateInk;
+    }
+
+    private void OnDisable()
+    {
+        PlayerControls.hitAcc -= CreateInk;
+        if (accuracy == ("Perfect"))
+        {
+            Instantiate(pSplatter, gameObject.transform.position, Quaternion.identity);
+            Instantiate(inkSplatter, gameObject.transform.position, Quaternion.identity);
+            Debug.Log("Making Perfect Effect");
+        }
+        else if (accuracy == ("Almost"))
+        {
+            Instantiate(pSplatter, gameObject.transform.position, Quaternion.identity);
+            Debug.Log("Making Almost Effect");
+        }
+    }
+
     private void Awake()
     {
         m_conductor = FindObjectOfType<Conductor>();
@@ -27,4 +51,29 @@ public class Monster : MonoBehaviour
     {
         transform.position = Vector3.Lerp(startPosition, targetPosition.position, Mathf.Clamp01((m_conductor.songPositionInBeats - spawnBeat - beatsToWait) / travelTimeInBeats));
     }
+
+    /*private void CreateInk(string Acc)
+    {
+        
+        if (Acc == ("Perfect"))
+        {
+            Instantiate(inkSplatter, gameObject.transform.position, Quaternion.identity);
+            pSplatter.Play();
+            Debug.Log("Making Perfect Effect");
+        }
+        else if(Acc == ("Almost"))
+        {
+            pSplatter.Play();
+            Debug.Log("Making Almost Effect");
+        }
+
+    }*/
+
+    public string accuracy; 
+
+    private void CreateInk(string Acc)
+    {
+        accuracy = Acc;
+    }
+
 }
